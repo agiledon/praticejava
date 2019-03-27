e9eb8ac06ce26fae137de614dd43967d1e406191
agiledon <15023157626@163.com>
2019-03-27 14:54:23 +0800

    add example of report

---
+++ design/src/main/java/xyz/zhangyi/practicejava/design/report/engine/Item.java
@@ -0,0 +1,14 @@
+package xyz.zhangyi.practicejava.design.report.engine;
+
+public class Item {
+    private String name;
+    private String[] values;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setValues(String[] values) {
+        this.values = values;
+    }
+}
---
+++ design/src/main/java/xyz/zhangyi/practicejava/design/report/engine/Parameter.java
@@ -0,0 +1,5 @@
+package xyz.zhangyi.practicejava.design.report.engine;
+
+public interface Parameter {
+    String getName();
+}
